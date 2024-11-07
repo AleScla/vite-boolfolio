1cbc9fa9f2d26fe684e24aee2a11051abaa74fa6
AleScla <scla.alessandro@gmail.com>
2024-11-07 16:04:57 +0100

    importato axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
+import axios from 'axios';
 
 
 export default {
