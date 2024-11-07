a14c6fe5a9961b95a0d360acfcb3896234afa3da
AleScla <scla.alessandro@gmail.com>
2024-11-07 16:05:16 +0100

    strutturata chiamata api index

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,17 @@
 <script>
 export default {
   data() {
-    
+    return {
+      pokemons = [],
+    }
+  },
+  methods:{
+    getPokemonData(){
+      axios.get('linkapi')
+          .then((res)=>{
+            this.pokemons = res.data
+          })
+    }
   }
 }
 </script>
@@ -11,7 +21,28 @@ export default {
     <div class="container">
       <div class="row">
           <div class="col">
-              
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Pokemon</th>
+                  <th scope="col">Tipo</th>
+                  <th scope="col">Azioni</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">ID pokemon</th>
+                  <td>nome poke</td>
+                  <td>nome tipo</td>
+                  <td>
+                    <router-link :to="{name:'show', params:{id:'idacaso'}}" class="btn btn-primary btn-sm">
+                      Visualizza info Pokemon
+                    </router-link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
       </div>
     </div>
